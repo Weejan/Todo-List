1f47b3e45515277c9fd663e4ab9c718ea8ee5080
weejan <weezan49@gmail.com>
2024-08-19 17:59:15 +0545

    Deploy try

--- vite-project/src/components/TodoList.jsx
+++ vite-project/src/components/TodoList.jsx
@@ -1,9 +1,12 @@
 import TodoItem from "./TodoItem.jsx";
 import styles from "./todolist.module.css";
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={styles.list}>
-      {todos.map((item) => {
+      {sortedTodos.map((item) => {
         return (
           <TodoItem
             key={item.name}
