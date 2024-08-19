cf9855676ccd56d5dcc424966ea96db8d36102df
weejan <weezan49@gmail.com>
2024-08-19 11:51:42 +0545

    Add delete button including its css and functionality

--- vite-project/src/components/TodoItem.jsx
+++ vite-project/src/components/TodoItem.jsx
@@ -1,10 +1,25 @@
 import style from "./todoitem.module.css";
 
-export default function TodoItem({ item }) {
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    setTodos(todos.filter((todo) => todo !== item));
+  }
   return (
     <>
       <div>
-        <div className={style.item}>{item}</div>
+        <div className={style.item} id={item}>
+          {item}
+          <span>
+            <button
+              onClick={() => {
+                handleDelete(item);
+              }}
+              className={style.button}
+            >
+              X
+            </button>
+          </span>
+        </div>
         <hr className={style.line} />
       </div>
     </>
